d3e3deb74d30ec51bc6877865807ab2eb4b179e5
ogjash <jashanpreet.singh.1004@gmail.com>
2026-06-20 18:05:55 +0530

    inset fixed

--- src/components/dashboard/inset-header.tsx
+++ src/components/dashboard/inset-header.tsx
@@ -1,20 +1,23 @@
+"use client"
+
 import { SearchIcon } from "lucide-react";
 import { Input } from "../ui/input";
 import HeaderTrigger from "./header-trigger";
 
 
 export default function Header() {
+
   return (
     <header className="fixed top-0 left-0 right-0 z-50 flex h-12 items-center justify-center px-4">
 
       <HeaderTrigger />
 
-      <div className="flex max-w-xs sm:max-w-md w-full relative pointer-events-auto ml-10 sm:ml-0">
+      <div className="flex flex-1 mx-16 sm:mx-0 sm:flex-initial sm:max-w-md w-full relative pointer-events-auto">
           <SearchIcon className="absolute m-2 h-3 w-3 text-sidebar-foreground/70" />
           <Input 
           type="search" 
-          placeholder="Search workspaces, subjects, or documents..." 
-          className="w-full bg-sidebar-accent/50 border-sidebar-border pl-9 text-sidebar-foreground placeholder:text-sidebar-foreground/50 focus-visible:border-blue-300 focus-visible:ring-0 h-7 text-xs sm:text-sm"
+          placeholder="Search" 
+          className="w-full bg-sidebar-accent/50 border-sidebar-border px-9 text-sidebar-foreground placeholder:text-sidebar-foreground/50 focus-visible:border-blue-300 focus-visible:ring-0 h-7 text-xs sm:text-sm"
           />
       </div>
     </header>
